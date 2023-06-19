c5c521e74eb72505057f81b17ff3d0926d69a2d5
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-06-19 16:44:19 +0200

    refactor(steps): add Template.html, refactor Home and Programm pages

--- src/es/components/organisms/eventList/EventList.js
+++ src/es/components/organisms/eventList/EventList.js
@@ -1,71 +1,212 @@
+// @ts-check
+import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
 import(
-  "../../web-components-toolbox/src/es/components/atoms/button/Button.js"
-).then((module) => self.customElements.define("a-button", module.default));
+  '../../web-components-toolbox/src/es/components/atoms/button/Button.js'
+// @ts-ignore
+).then((module) => self.customElements.define('a-button', module.default))
 
-class EventList extends HTMLElement {
-  constructor() {
-    super();
-    this.events = [];
+/**
+ * EventList
+ * An example at: src/es/components/pages/Program.html
+ *
+ * @export
+ * @class EventList
+ * @type {CustomElementConstructor}
+ */
+
+export default class EventList extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+    this.events = []
   }
 
-  connectedCallback() {
-    const jsonUrl = this.getAttribute("json-url");
+  connectedCallback () {
+    if (this.shouldRenderCSS()) this.renderCSS()
+    const jsonUrl = this.getAttribute('json-url')
+    // @ts-ignore
     fetch(jsonUrl)
       .then((response) => response.json())
       .then((data) => {
-        this.events = data;
-        this.render();
+        this.events = data
+        this.renderHTML()
       })
       .catch((error) => {
-        console.error(error);
-      });
+        console.error(error)
+      })
   }
 
-  render() {
-    let eventHtml = "";
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
+  }
+
+  renderCSS () {
+    this.css = /* css */`
+      :host {
+        --button-secondary-width: 100%;
+        --event-date-font-size: calc(2.5rem + 1vw);
+      }
+      :host .event-list {
+        list-style: none;
+        margin: 1.25rem 0;
+        padding-left: 0;
+      }
+      :host .event-item {
+        border-top: 1px solid var(--steps-color-black);
+        padding: 1.25rem 0;
+        display: flex;
+        flex-direction: row;
+        gap: 2rem;
+        position: relative;
+      }
+      :host .event-item:last-child {
+        border-bottom: 1px solid var(--steps-color-black)
+      }
+      :host .event-item p {
+        margin: 0;
+      }
+      :host .event-date {
+        white-space: nowrap;
+        min-width: 28vw;
+      }
+      :host .event-date p {
+        font-size: var(--event-date-font-size);
+      }
+      :host .event-date span {
+        display: block;
+        font-size: 1.4rem;
+      }
+      :host .event-info {
+        display: flex;
+        flex-direction: column;
+        gap: 1.25rem;
+        width: 100%;
+      }
+      :host .event-info > * {
+        flex-grow: 0;
+        flex-shrink: 0;
+        hyphens: auto;
+      }
+      :host .legend-icons {
+        display: inline-block;
+        margin-right: 1.25rem;
+        white-space: nowrap;
+      }
+      :host .event-cta {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        gap: 1.25rem;
+        padding: 1.25rem 0;
+        font-size: var(--font-size);
+      }
+      :host .event-cta a-button {
+        width: 100%;
+        white-space: nowrap;
+      }
+      :host .event-cta a {
+        padding-top: var(--button-secondary-padding) ;
+      }
+      :host .sold-out {
+        color: var(--m-gray-400);
+        cursor: not-allowed;
+      }
+      :host .sold-out .event-cta,
+      :host .sold-out img[src*="svg"] {
+        opacity: 0.4;
+      }
+      :host .sold-out .event-cta a-button {
+        display: none;
+      }
+      :host .badge-sold-out {
+        background-color: var(--steps-color-white);
+        border: 2px dashed var(--steps-color-red);
+        color: var(--steps-color-red);
+        display: inline-block;
+        padding: 1.25rem;
+        position: absolute;
+        top: calc(50% - 32.5px);
+        left: calc(50% - 100px);
+        text-align: center;
+        transform: rotate(2deg);
+        width: 200px;
+        z-index: 2;
+      }
+      
+      @media only screen and (min-width: 768px)  {
+        :host .event-item { 
+          gap: 4rem;
+        }
+        :host .event-date {
+          min-width: 10vw;
+        }
+        :host .event-info {
+          flex-direction: row;
+          justify-content: space-between;
+        }
+        :host .event-info > *:first-child {
+          flex-basis: 25%;
+        }
+        :host .event-info > *:not(:first-child) {
+          flex-basis: 20%;
+        }
+        :host .event-cta {
+          align-items: center;
+          padding: 0;
+        }
+      }
+    `
+  }
+
+  renderHTML () {
+    let eventHtml = ''
     this.events.forEach((event) => {
       /* date and time */
-      const timestamp = event.timestamp;
-      const weekDay = new Date(timestamp * 1000).toLocaleDateString("de-CH", {
-        weekday: "long",
-      });
-      const dateShort = new Date(timestamp * 1000).toLocaleDateString("de-CH", {
-        day: "numeric",
-        month: "numeric",
-      });
-      const time = new Date(timestamp * 1000).toLocaleTimeString("de-CH", {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
+      const timestamp = event.timestamp
+      const weekDay = new Date(timestamp * 1000).toLocaleDateString('de-CH', {
+        weekday: 'long'
+      })
+      const dateShort = new Date(timestamp * 1000).toLocaleDateString('de-CH', {
+        day: 'numeric',
+        month: 'numeric'
+      })
+      const time = new Date(timestamp * 1000).toLocaleTimeString('de-CH', {
+        hour: '2-digit',
+        minute: '2-digit'
+      })
 
       /* icons */
-      const eventIconsArray = event.icons;
-      let eventIcons = "";
+      const eventIconsArray = event.icons
+      let eventIcons = ''
       for (const icon of eventIconsArray) {
         eventIcons +=
-          '<img src="../../../img/icons/icon-' +
+          '<img src="../../img/icons/icon-' +
           icon +
           '.svg" class="legend-icon-' +
           icon +
-          '" width="24" height="24" />';
+          '" width="24" height="24" />'
       }
-      const locationIconsArray = event.location_icons;
-      let locationIcons = "";
+      const locationIconsArray = event.location_icons
+      let locationIcons = ''
       for (const icon of locationIconsArray) {
         locationIcons +=
-          '<img src="../../../img/icons/icon-' +
+          '<img src="../../img/icons/icon-' +
           icon +
           '.svg" class="legend-icon-' +
           icon +
-          '" width="24" height="24" />';
+          '" width="24" height="24" />'
       }
 
-      eventHtml += /*html*/ `
-          <li class="${event.sold_out ? "sold-out event-item" : "event-item"}">
+      eventHtml += /* html */ `
+          <li class="${event.sold_out ? 'sold-out event-item' : 'event-item'}">
             ${
               event.sold_out
-                ? '<p class="badge-sold-out">aus&shy;verkauft</p>'
-                : ""
+                ? '<p class="badge-sold-out">ausverkauft</p>'
+                : ''
             }
             <div class="event-date">
               <p><span>${weekDay}</span>${dateShort}</p>
@@ -95,130 +236,15 @@ class EventList extends HTMLElement {
               </p>
             </div>
           </li>
-        `;
-    });
+        `
+    })
 
-    this.innerHTML = /* html */ `
-        <style>
-          :host {
-            --button-secondary-width: 100%;
-            --event-date-font-size: calc(2.5rem + 1vw);
-          }
-          :host .event-list {
-            list-style: none;
-            margin: 1.25rem 0;
-            padding-left: 0;
-          }
-          :host .event-item {
-            border-top: 1px solid var(--steps-color-black);
-            padding: 1.25rem 0;
-            display: flex;
-            flex-direction: row;
-            gap: 2rem;
-            position: relative;
-          }
-          :host .event-item:last-child {
-            border-bottom: 1px solid var(--steps-color-black)
-          }
-          :host .event-item p {
-            margin: 0;
-          }
-          :host .event-date {
-            white-space: nowrap;
-            min-width: 28vw;
-          }
-          :host .event-date p {
-            font-size: var(--event-date-font-size);
-          }
-          :host .event-date span {
-            display: block;
-            font-size: 1.4rem;
-          }
-          :host .event-info {
-            display: flex;
-            flex-direction: column;
-            gap: 1.25rem;
-            width: 100%;
-          }
-          :host .event-info > * {
-            flex-grow: 0;
-            flex-shrink: 0;
-            hyphens: auto;
-          }
-          :host .legend-icons {
-            display: inline-block;
-            margin-right: 1.25rem;
-            white-space: nowrap;
-          }
-          :host .event-cta {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            gap: 1.25rem;
-            padding: 1.25rem 0;
-            font-size: var(--font-size);
-          }
-          :host .event-cta a-button {
-            width: 100%;
-            white-space: nowrap;
-          }
-          :host .event-cta a {
-            padding-top: var(--button-secondary-padding) ;
-          }
-          :host .sold-out {
-            color: var(--m-gray-400);
-            cursor: not-allowed;
-          }
-          :host .sold-out .event-cta,
-          :host .sold-out img[src*="svg"] {
-            opacity: 0.4;
-          }
-          :host .sold-out .event-cta a-button {
-            display: none;
-          }
-          :host .badge-sold-out {
-            background-color: var(--steps-color-white);
-            border: 2px dashed var(--steps-color-red);
-            color: var(--steps-color-red);
-            display: inline-block;
-            padding: 1.25rem;
-            position: absolute;
-            top: calc(50% - 32.5px);
-            left: calc(50% - 100px);
-            text-align: center;
-            transform: rotate(2deg);
-            width: 200px;
-            z-index: 2;
-          }
-          
-          @media only screen and (min-width: 768px)  {
-            :host .event-item { 
-              gap: 4rem;
-            }
-            :host .event-date {
-              min-width: 10vw;
-            }
-            :host .event-info {
-              flex-direction: row;
-              justify-content: space-between;
-            }
-            :host .event-info > *:first-child {
-              flex-basis: 25%;
-            }
-            :host .event-info > *:not(:first-child) {
-              flex-basis: 20%;
-            }
-            :host .event-cta {
-              align-items: center;
-              padding: 0;
-            }
-          }
-        </style>
+    this.html = /* html */ `
         <ul class="event-list">
           ${eventHtml}
         </ul>
-      `;
+      `
   }
 }
 
-customElements.define("event-list", EventList);
+// customElements.define('event-list', EventList)
