20acc3f6da3437c722bfe55488235e123b616d27
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-06-29 15:09:20 +0200

    refactor(EventCard.js): restructure constructor to initialize additional properties feat(EventCard.js): add support for location and translation texts refactor(EventCard.js): update CSS styles for event list refactor(EventCard.js): update comments to use single-line comments
    refactor(EventCard.js): refactor icon generation logic into a separate function
    refactor(EventCard.js): refactor button text to use translation texts
    refactor(EventCard.js): refactor event and location information to use updated property names
    refactor(EventCard.js): refactor event card HTML structure for better readability
    
    chore(EventList.js): update example file path from src/es/components/pages/Program.html to src/es/components/pages/Spielplan.html
    feat(EventList.js): add dynamic import for EventCard component and define custom element 'm-steps-event-card'
    
    refactor(EventList.js):
    - Remove unused CSS styles for event list and event item
    - Update HTML rendering to use a custom component for each event item
    
    chore(eventList): update example-events.json with new event data
    
    chore(es): update event details in Event.html
    feat(es): add choreographer and company information to event cards
    feat(es): update location icons in event cards
    feat(es): add production information to event cards
    feat(es): update text labels in event cards
    feat(es): update timestamp format in event cards
    feat(es): update title and title subline in event cards
    
    feat(icons): add rollstuhlgaengig.svg icon file

--- src/es/components/molecules/eventCard/EventCard.js
+++ src/es/components/molecules/eventCard/EventCard.js
@@ -18,19 +18,30 @@ export default class EventCard extends Shadow() {
   constructor (options = {}, ...args) {
     super({ importMetaUrl: import.meta.url, ...options }, ...args)
     this.event = {}
+    this.location = {}
+    this.translationTexts = {}
   }
 
   connectedCallback () {
     if (this.shouldRenderCSS()) this.renderCSS()
     this.event = {
-      icons: JSON.parse(this.getAttribute('icons')),
-      location: this.getAttribute('location'),
-      locationIcons: JSON.parse(this.getAttribute('location-icons')),
-      locationSubline: this.getAttribute('location-subline'),
-      soldOut: this.getAttribute('sold-out') === 'true',
+      choreographer: this.getAttribute('choreographer'),
+      company: this.getAttribute('company'),
+      icons: this.getAttribute('eventIcons'),
+      production: this.getAttribute('production'),
+      soldOut: this.getAttribute('soldOut') === 'true',
       timestamp: this.getAttribute('timestamp'),
-      title: this.getAttribute('title'),
-      titleSubline: this.getAttribute('title-subline')
+    }
+    this.location = {
+      name: this.getAttribute('location'),
+      icons: this.getAttribute('locationIcons'),
+      subline: this.getAttribute('locationSubline'),
+    }
+    this.translationTexts = {
+      buttonTickets: this.getAttribute('textButtonTickets'),
+      linkDetails: this.getAttribute('textLinkDetails'),
+      soldOut: this.getAttribute('textSoldOut'),
+      timeSuffix: this.getAttribute('textTimeSuffix'),
     }
     this.renderHTML()
   }
@@ -51,11 +62,6 @@ export default class EventCard extends Shadow() {
       :host {
         --button-secondary-width: 100%;
         --button-secondary-font-size: 1rem;
-      }
-      :host .event-list {
-        list-style: none;
-        margin: 1.25rem 0;
-        padding-left: 0;
       }
       :host .event-item {
         border-top: 1px solid var(--steps-color-black);
@@ -140,7 +146,7 @@ export default class EventCard extends Shadow() {
   renderHTML () {
     let eventHtml = ''
 
-    /* date and time */
+    // date and time
     const weekDay = new Date(this.event.timestamp * 1000).toLocaleDateString(
       'de-CH',
       {
@@ -162,69 +168,59 @@ export default class EventCard extends Shadow() {
       }
     )
 
-    /* icons */
-    const eventIconsArray = this.event.icons
-    let eventIcons = ''
-    for (const icon of eventIconsArray) {
-      eventIcons +=
-        '<img src="../../../../img/icons/icon-' +
-        icon +
-        '.svg" class="legend-icon-' +
-        icon +
-        '" width="24" height="24" />'
-    }
-    const locationIconsArray = this.event.locationIcons
-    let locationIcons = ''
-    for (const icon of locationIconsArray) {
-      locationIcons +=
-        '<img src="../../../../img/icons/icon-' +
-        icon +
-        '.svg" class="legend-icon-' +
-        icon +
-        '" width="24" height="24" />'
-    }
+    // icons
+    const generateIconHTML = (iconsArray) => {
+      let icons = '';
+      for (const icon of iconsArray) {
+        icons += `<img src="../../../../img/icons/icon-${icon}.svg" class="legend-icon-${icon}" width="24" height="24" />`;
+      }
+      return icons;
+    };
 
-    /* buttons */
-    const ctaButton = `<a-button namespace="button-secondary-">Tickets &#8594;</a-button>`
-    const soldOutButton = `<a-button namespace="button-secondary-">ausverkauft</a-button>`
+    const eventIcons = generateIconHTML(JSON.parse(this.event.icons));
+    const locationIcons = generateIconHTML(JSON.parse(this.location.icons));
+
+    // buttons
+    const ctaButton = `<a-button namespace="button-secondary-">${this.translationTexts.buttonTickets} &#8594;</a-button>`
+    const soldOutButton = `<a-button namespace="button-secondary-">${this.translationTexts.soldOut}</a-button>`
 
 
     eventHtml += /* html */ `
-          <div class="event-item">
-            <div class="event-date">
-              <p>${weekDay}<br /><span>${dateShort}</span></p>
-            </div>
-            <div class="event-info">
-              <p>
-                <strong>${this.event.title}</strong><br />
-                <span>${this.event.titleSubline}</span>
-              </p>
-              <p>
-                <strong>${this.event.location}</strong><br />
-                <span>${this.event.locationSubline}</span>
-              </p>
-              <p>
-                ${time} Uhr<br />
-                <br />
-                <span class="legend-icons">
-                  ${eventIcons}
-                </span>
-                <span class="legend-icons">
-                  ${locationIcons}
-                </span>
-              </p>
-              <p class="event-cta">
-                ${this.event.soldOut ? soldOutButton : ctaButton}
-                <a href="#">Mehr Details</a>
-              </p>
-            </div>
-          </div>
-        `
+      <div class="event-item">
+        <div class="event-date">
+          <p>${weekDay}<br /><span>${dateShort}</span></p>
+        </div>
+        <div class="event-info">
+          <p>
+            <strong>${this.event.company}</strong><br />
+            <span>${this.event.production}<br />${this.event.choreographer}</span>
+          </p>
+          <p>
+            <strong>${this.location.name}</strong><br />
+            <span>${this.location.subline}</span>
+          </p>
+          <p>
+            ${time} ${this.translationTexts.timeSuffix}<br />
+            <br />
+            <span class="legend-icons">
+              ${eventIcons}
+            </span>
+            <span class="legend-icons">
+              ${locationIcons}
+            </span>
+          </p>
+          <p class="event-cta">
+            ${this.event.soldOut ? soldOutButton : ctaButton}
+            <a href="#">${this.translationTexts.linkDetails}</a>
+          </p>
+        </div>
+      </div>
+    `
 
     this.html = /* html */ `
-        <div class="event-card">
-          ${eventHtml}
-        </div>
-      `
+      <div class="event-card">
+        ${eventHtml}
+      </div>
+    `
   }
 }
--- src/es/components/organisms/eventList/EventList.js
+++ src/es/components/organisms/eventList/EventList.js
@@ -1,9 +1,13 @@
 // @ts-check
 import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+import(
+  '../../molecules/eventCard/EventCard.js'
+  // @ts-ignore
+).then((module) => customElements.define('m-steps-event-card', module.default))
 
 /**
  * EventList
- * An example at: src/es/components/pages/Program.html
+ * An example at: src/es/components/pages/Spielplan.html
  *
  * @export
  * @class EventList
@@ -47,173 +51,32 @@ export default class EventList extends Shadow() {
         --button-secondary-font-size: 1rem;
       }
       :host .event-list {
-        list-style: none;
         margin: 1.25rem 0;
-        padding-left: 0;
-      }
-      :host .event-item {
-        border-top: 1px solid var(--steps-color-black);
-        padding: 1.25rem 0;
-        display: flex;
-        flex-direction: row;
-        gap: 1rem;
-        position: relative;
-      }
-      :host .event-item:last-child {
-        border-bottom: 1px solid var(--steps-color-black)
-      }
-      :host .event-item p {
-        margin: 0;
-      }
-      :host .event-date {
-        white-space: nowrap;
-        min-width: 28vw;
-      }
-      :host .event-date p {
-        font-size: var(--event-date-font-size);
-      }
-      :host .event-date span {
-        display: block;
-        font-size: 1.8rem;
-      }
-      :host .event-info {
-        display: flex;
-        flex-direction: column;
-        gap: 1.25rem;
-        width: 100%;
-      }
-      :host .event-info > * {
-        flex-grow: 0;
-        flex-shrink: 0;
-        hyphens: auto;
-      }
-      :host .legend-icons {
-        display: inline-block;
-        margin-right: 1.25rem;
-        white-space: nowrap;
-      }
-      :host .event-cta {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        gap: 1.25rem;
-        padding: 1.25rem 0;
-        font-size: 1rem;
-      }
-      :host .event-cta a-button {
-        width: 100%;
-        white-space: nowrap;
-      }
-      :host .event-cta a {
-        color: var(--steps-color-black);
-        padding-top: var(--button-secondary-padding) ;
-      }
-      
-      @media only screen and (min-width: 768px)  {
-        :host .event-item { 
-          gap: 2rem;
-        }
-        :host .event-date {
-          min-width: 10vw;
-        }
-        :host .event-info {
-          flex-direction: row;
-          justify-content: space-between;
-        }
-        :host .event-info > *:first-child {
-          flex-basis: 25%;
-        }
-        :host .event-info > *:not(:first-child) {
-          flex-basis: 20%;
-        }
-        :host .event-cta {
-          align-items: center;
-          padding: 0;
-        }
       }
     `
   }
 
   renderHTML () {
-    let eventHtml = ''
-    this.events.forEach((event) => {
-      /* date and time */
-      const timestamp = event.timestamp
-      const weekDay = new Date(timestamp * 1000).toLocaleDateString('de-CH', {
-        weekday: 'long'
-      })
-      const dateShort = new Date(timestamp * 1000).toLocaleDateString('de-CH', {
-        day: 'numeric',
-        month: 'numeric'
-      })
-      const time = new Date(timestamp * 1000).toLocaleTimeString('de-CH', {
-        hour: '2-digit',
-        minute: '2-digit'
-      })
-
-      /* icons */
-      const eventIconsArray = event.icons
-      let eventIcons = ''
-      for (const icon of eventIconsArray) {
-        eventIcons +=
-          '<img src="../../../../img/icons/icon-' +
-          icon +
-          '.svg" class="legend-icon-' +
-          icon +
-          '" width="24" height="24" />'
-      }
-      const locationIconsArray = event.locationIcons
-      let locationIcons = ''
-      for (const icon of locationIconsArray) {
-        locationIcons +=
-          '<img src="../../../../img/icons/icon-' +
-          icon +
-          '.svg" class="legend-icon-' +
-          icon +
-          '" width="24" height="24" />'
-      }
-
-      /* buttons */
-      const ctaButton = `<a-button namespace="button-secondary-">Tickets &#8594;</a-button>`
-      const soldOutButton = `<a-button namespace="button-secondary-">ausverkauft</a-button>`
-
-      eventHtml += /* html */ `
-          <li class="event-item">
-            <div class="event-date">
-              <p>${weekDay}<br /><span>${dateShort}</span></p>
-            </div>
-            <div class="event-info">
-              <p>
-                <strong>${event.title}</strong><br />
-                <span>${event.titleSubline}</span>
-              </p>
-              <p>
-                <strong>${event.location}</strong><br />
-                <span>${event.locationSubline}</span>
-              </p>
-              <p>
-                ${time} Uhr<br />
-                <br />
-                <span class="legend-icons">
-                  ${eventIcons}
-                </span>
-                <span class="legend-icons">
-                  ${locationIcons}
-                </span>
-              </p>
-              <p class="event-cta">
-                ${event.soldOut ? soldOutButton : ctaButton}
-                <a href="#">Mehr Details</a>
-              </p>
-            </div>
-          </li>
-        `
-    })
+    const eventHtml = this.events.map(item => /* html */`
+      <m-steps-event-card 
+        choreographer="${item.event.choreographer}"
+        company="${item.event.company}"
+        eventIcons='${JSON.stringify(item.event.icons)}'
+        location="${item.location.name}"
+        locationIcons='${JSON.stringify(item.location.icons)}'
+        locationSubline="${item.location.subline}"
+        production="${item.event.production}"
+        textButtonTickets="${item.translationTexts.buttonTickets}"
+        textLinkDetails="${item.translationTexts.linkDetails}"
+        textSoldOut="${item.translationTexts.soldOut}"
+        textTimeSuffix="${item.translationTexts.timeSuffix}"
+        timestamp="${item.event.timestamp}"
+      ></m-steps-event-card>`).join('');
 
     this.html = /* html */ `
-        <ul class="event-list">
-          ${eventHtml}
-        </ul>
-      `
+      <div class="event-list">
+        ${eventHtml}
+      </div>
+    `
   }
 }
