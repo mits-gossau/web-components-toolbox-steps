46e9ab189325360fa218cb63f714fb6e6ed6f52f
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-07-11 09:05:55 +0200

    style(EventCard.js): fix import statement formatting
    refactor(EventCard.js): update property names and types in the EventCard class constructor
    feat(EventCard.js): add new properties to the EventCard class constructor
    
    refactor(EventCard.js):
    - Remove unused variables `eventIcons` and `locationIcons`
    - Rename `compagnieName` to `company`
    - Rename `productionTitle` to `production`
    - Rename `dateTime` to `eventDate`
    - Rename `location` to `theater`
    - Remove `subline` from `location`
    - Remove `icons` from `event` and `location`
    - Update references to renamed variables and properties
    - Update `shouldRenderCSS` method to use `this.root` instead of `this`
    - Update `renderCSS` method to use template literals for CSS
    - Update `renderHTML` method to use renamed `eventDate` property
    
    refactor(EventCard.js): refactor variable names for clarity and consistency
    feat(EventCard.js): add support for theater information icons
    feat(EventCard.js): add support for sold out button
    feat(EventCard.js): add support for conditional rendering of sold out button
    feat(EventCard.js): update event and location information variables to match API response
    
    refactor(EventList.js): fix import statements formatting
    feat(EventList.js): define custom element 'm-steps-event-card' for EventCard module
    refactor(EventList.js): fix constructor formatting and add missing semicolons
    
    refactor(EventList.js): clean up code formatting and add missing semicolons
    feat(EventList.js): add event listeners for fetching data and rendering HTML
    
    refactor(EventList.js): change shouldRenderCSS method to use multi-line querySelector
    refactor(EventList.js): change renderCSS method to use multi-line css string
    
    refactor(EventList.js): improve code formatting and variable naming
    feat(EventList.js): update renderHTML method to use destructuring and improve readability

--- src/es/components/molecules/eventCard/EventCard.js
+++ src/es/components/molecules/eventCard/EventCard.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+import { Shadow } from "../../web-components-toolbox/src/es/components/prototypes/Shadow.js";
 
 /**
  * EventCard
@@ -11,43 +11,34 @@ import { Shadow } from '../../web-components-toolbox/src/es/components/prototype
  */
 
 export default class EventCard extends Shadow() {
-  constructor (options = {}, ...args) {
-    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+  constructor(options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args);
     /**
      * @typedef {Object} Event
-     * @property {string} choreographer - The name of the choreographer for the event.
-     * @property {string} compagnieName - The name of the company associated with the event.
-     * @property {string[]} icons - An array of icons associated with the event.
-     * @property {string} productionTitle - The title of the production associated with the event.
-     * @property {boolean} soldOut - Indicates if the event is sold out.
-     * @property {string} dateTime - The date and time of the event in the format "DD.MM.YYYY HH:mm:ss".
+     * @property {string} choreographer - The name of the choreographer.
+     * @property {string} company - The name of the company.
+     * @property {string} eventDate - The date and time of the event.
+     * @property {string[]} eventInformationIcons - An array of event information icons.
+     * @property {string} location - The location name.
+     * @property {string} production - The production title.
+     * @property {string} soldOut - Indicates whether the event is sold out.
+     * @property {string} theater - The theater name.
+     * @property {string[]} theaterInformationIcons - An array of theater information icons.
      */
     /**
      * @type {Event}
      */
     this.event = {
-      choreographer: '',
-      compagnieName: '',
-      icons: [],
-      productionTitle: '',
-      soldOut: false,
-      dateTime: '',
-    }
-    /**
-     * @typedef {Object} Location
-     * @property {string} name - The location name (e.g., "Zürich").
-     * @property {string} subline - Additional information about the location (e.g., "Théatre du Passage, ADN").
-     * @property {string[]} icons - An array of icons associated with the location (e.g., ["hoerbenachteiligung", "rollstuhlgaengig"]).
-     */
-    
-    /**
-     * @type {Location}
-     */
-    this.location = {
-      name: '',
-      subline: '',
-      icons: [],
-    }
+      choreographer: "",
+      company: "",
+      eventDate: "",
+      eventInformationIcons: [],
+      location: "",
+      production: "",
+      soldOut: "",
+      theater: "",
+      theaterInformationIcons: [],
+    };
   }
 
   connectedCallback() {
@@ -55,24 +46,21 @@ export default class EventCard extends Shadow() {
       this.renderCSS();
     }
 
-    // icons
-    const eventIcons = JSON.parse(this.getAttribute('eventIcons'));
-    const locationIcons = JSON.parse(this.getAttribute('locationIcons'));
-
     // properties
     this.event = {
-      choreographer: this.getAttribute('choreographer'),
-      compagnieName: this.getAttribute('compagnieName'),
-      icons: eventIcons,
-      productionTitle: this.getAttribute('productionTitle'),
-      soldOut: this.getAttribute('soldOut') === 'True',
-      dateTime: this.getAttribute('dateTime'),
-    };
-
-    this.location = {
-      name: this.getAttribute('location'),
-      icons: locationIcons,
-      subline: this.getAttribute('locationSubline'),
+      choreographer: this.getAttribute("choreographer"),
+      company: this.getAttribute("company"),
+      eventDate: this.getAttribute("eventDate"),
+      eventInformationIcons: JSON.parse(
+        this.getAttribute("eventInformationIcons")
+      ),
+      location: this.getAttribute("location"),
+      production: this.getAttribute("production"),
+      soldOut: this.getAttribute("soldOut"),
+      theater: this.getAttribute("theater"),
+      theaterInformationIcons: JSON.parse(
+        this.getAttribute("theaterInformationIcons")
+      ),
     };
 
     this.renderHTML();
@@ -83,13 +71,13 @@ export default class EventCard extends Shadow() {
    *
    * @return {boolean}
    */
-  shouldRenderCSS () {
+  shouldRenderCSS() {
     return !this.root.querySelector(
       `:host > style[_css], ${this.tagName} > style[_css]`
-    )
+    );
   }
 
-  renderCSS () {
+  renderCSS() {
     this.css = /* css */ `
       :host {
         --button-secondary-width: 100%;
@@ -172,12 +160,12 @@ export default class EventCard extends Shadow() {
           padding: 0;
         }
       }
-    `
+    `;
   }
 
-  renderHTML () {
+  renderHTML() {
     // date and time
-    const dateString = this.event.dateTime;
+    const dateString = this.event.eventDate;
     const dateParts = dateString.split(".");
     const timeParts = dateParts[2].split(" ");
     const day = parseInt(dateParts[0]);
@@ -186,50 +174,64 @@ export default class EventCard extends Shadow() {
     const eventTime = timeParts[1];
     const [hours, minutes, seconds] = eventTime.split(":").map(Number);
     const eventTimestamp = new Date(year, month, day, hours, minutes, seconds);
-    
-    const weekDay = eventTimestamp.toLocaleDateString('de-CH', { weekday: 'long' });
-    const dateShort = eventTimestamp.toLocaleDateString('de-CH', { day: 'numeric', month: 'numeric' });
-    const time = eventTimestamp.toLocaleTimeString('de-CH', { hour: '2-digit', minute: '2-digit' });
+
+    const weekDay = eventTimestamp.toLocaleDateString("de-CH", {
+      weekday: "long",
+    });
+    const dateShort = eventTimestamp.toLocaleDateString("de-CH", {
+      day: "numeric",
+      month: "numeric",
+    });
+    const time = eventTimestamp.toLocaleTimeString("de-CH", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
 
     // icons
     const generateIconHTML = (iconsArray) => {
-      let icons = '';
+      let icons = "";
       for (const icon of iconsArray) {
         icons += `<img src="../../../../img/icons/icon-${icon}.svg" class="legend-icon-${icon}" width="24" height="24" />`;
       }
       return icons;
     };
 
-    const eventIcons = generateIconHTML(this.event.icons);
-    const locationIcons = generateIconHTML(this.location.icons);
+    const eventIcons = generateIconHTML(this.event.eventInformationIcons);
+    const theaterIcons = generateIconHTML(this.event.theaterInformationIcons);
 
     // buttons
-    const ctaButton = `<a-button namespace="button-secondary-">${this.getAttribute('textButtonTickets')} &#8594;</a-button>`
-    const soldOutButton = `<a-button namespace="button-secondary-">${this.getAttribute('textSoldOut')}</a-button>`
+    const buttonTickets = `<a-button namespace="button-secondary-">${this.getAttribute(
+      "textButtonTickets"
+    )} &#8594;</a-button>`;
+    const buttonSoldOut = `<a-button namespace="button-secondary-">${this.getAttribute(
+      "textSoldOut"
+    )}</a-button>`;
+    const buttonCta =
+      this.event.soldOut === "True" ? buttonSoldOut : buttonTickets;
 
     const eventInfoHtml = /* html */ `
       <div class="event-info">
         <p>
-          <strong>${this.event.compagnieName}</strong><br />
-          <span>${this.event.productionTitle}<br />${this.event.choreographer}</span>
+          <strong>${this.event.company}</strong><br />
+          <span>${this.event.production}<br />${this.event.choreographer}</span>
         </p>
         <p>
-          <strong>${this.location.name}</strong><br />
-          <span>${this.location.subline}</span>
+          <strong>${this.event.location}</strong><br />
+          <span>${this.event.theater}</span>
         </p>
         <p>
-          ${time} ${this.getAttribute('textTimeSuffix')}<br />
+          ${time} ${this.getAttribute("textTimeSuffix")}<br />
           <br />
           <span class="legend-icons">
             ${eventIcons}
           </span>
           <span class="legend-icons">
-            ${locationIcons}
+            ${theaterIcons}
           </span>
         </p>
         <p class="event-cta">
-          ${this.event.soldOut ? soldOutButton : ctaButton}
-          <a href="#">${this.getAttribute('textLinkDetails')}</a>
+          ${buttonCta}
+          <a href="#">${this.getAttribute("textLinkDetails")}</a>
         </p>
       </div>
     `;
