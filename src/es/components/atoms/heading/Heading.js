363edfe8ae393bc7c2e1a2979e255fe0b07288c8
sturudic <srdan.turudic@emea.merkleinc.com>
2023-08-18 15:48:54 +0200

    fix: various frontend issues

--- src/es/components/atoms/heading/Heading.js
+++ src/es/components/atoms/heading/Heading.js
@@ -19,6 +19,7 @@ export default class Heading extends Shadow() {
      * @property {string} tag - The heading tag.
      * @property {string} type - The heading type.
      * @property {string} text - The heading text.
+     * @property {string} className - A heading custom class.
      * @property {boolean} isSmall - The heading text styling small.
      */
     /**
@@ -28,6 +29,7 @@ export default class Heading extends Shadow() {
         tag: 'h1',
         type: 'h1',
         text: 'I am a default heading',
+        className: '',
         isSmall: false
     }
   }
@@ -36,13 +38,14 @@ export default class Heading extends Shadow() {
     if (this.shouldRenderCSS()) this.renderCSS()
 
     this.heading = {
-        tag: this.getAttribute('tag'),
-        type: this.getAttribute('type'),
-        text: this.getAttribute('text'),
+        tag: this.getAttribute('tag') || this.heading.tag,
+        type: this.getAttribute('type') || this.heading.type,
+        text: this.getAttribute('text') || this.heading.text,
+        className: this.getAttribute('className'),
         isSmall: this.getAttribute('isSmall') || false
     }
 
-    this.renderHTML()
+    if (this.shouldRenderHTML()) this.renderHTML()
   }
 
   /**
@@ -54,12 +57,22 @@ export default class Heading extends Shadow() {
     return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
   }
 
+  /**
+ * evaluates if a render is necessary
+ *
+ * @return {boolean}
+ */
+  shouldRenderHTML () {
+    return !this.root.querySelector('.heading');
+  }
+
   renderCSS () {
     this.css = /* css */`
         :host .heading {
             font-size: var(--steps-h-font-size);
             margin: 0;
             padding-bottom: 2rem;
+            font-family: "HelveticaNowText";
         }
 
         :host .heading--h1 {
@@ -82,7 +95,7 @@ export default class Heading extends Shadow() {
 
   renderHTML () {
     this.html = /* html */ `
-        <${this.heading.tag} class="heading heading--${this.heading.type} ${this.heading.isSmall ? `heading--small` : ''}">
+        <${this.heading.tag} class="heading heading--${this.heading.type}${this.heading.className ? ` ${this.heading.className}` : ''}${this.heading.isSmall ? ` heading--small` : ''}">
             ${this.heading.text}
         </${this.heading.tag}>
     `
--- src/es/components/molecules/addresses/Addresses.js
+++ src/es/components/molecules/addresses/Addresses.js
@@ -46,11 +46,37 @@ export default class Addresses extends Shadow() {
             padding-bottom: var(--component-spacing);
             display: flex;
             flex-wrap: wrap;
+            font-family: "HelveticaNowText";
         }
         
         :host .address-item {
             width: 50%;
             margin-bottom: 2rem;
+            font-style: normal;
+        }
+
+        :host p {
+          margin: 0;
+        }
+
+        :host .address-name {
+          color: var(--steps-color-blue);
+          margin: 0;
+        }
+
+        :host .address-phone {
+          color: var(--steps-color-black);
+          text-decoration: none;
+        }
+
+        :host .address-link {
+          color: var(--steps-color-black);
+          font-family: "HelveticaNowTextBold";
+        }
+
+        :host .address-icons {
+          display: inline-block;
+          margin-top: 1rem;
         }
 
         @media (max-width: 768px) {
@@ -69,14 +95,18 @@ export default class Addresses extends Shadow() {
   renderHTML (addresses) {
     let addressesMarkup = '';
     addresses.forEach(address => {
-      let newAddress = `
+      let newAddress = /* html */`
         <address class="address-item">
-          <p>${address.theatercity}</p>
-          <h2>${address.theatername}</h2>
-          <p>${address.theaterstreet}<br />
-          <a href="tel:${address.theaterphone}">${address.theaterphone}</a><br />
-          <strong><a href="${address.theaterurl}" target="_blank">${address.theaterurl}</a></strong><br />
+          <p class="address-city">${address.theatercity}</p>
+          <h2 class="address-name">${address.theatername}</h2>
+          <p class="address-street">${address.theaterstreet}<br />
+          <a class="address-phone" href="tel:${address.theaterphone}">${address.theaterphone}</a><br />
+          <strong><a class="address-link" href="${address.theaterurl}" target="_blank">${address.theaterurltitle} →</a></strong><br />
           </p>
+          ${address.theaterInformationIcons
+            ? `<p><span class="address-icons">${this.collectIcons(address.theaterInformationIcons)}</span></p>`
+            : ''
+          }
         </address>        
       `;
 
@@ -85,5 +115,25 @@ export default class Addresses extends Shadow() {
     });
 
     this.html = addressesMarkup;
+  }
+
+  /**
+   * Loop icons and collect markup
+   * 
+   * @param {*} icons 
+   * @returns iconMarkup
+   */
+  collectIcons (icons) {
+    let iconMarkup = '';
+    icons.forEach(icon => {
+      let newIcon = /* html */`
+        <img src="${icon}" width="24" height="24">
+      `;
+
+      iconMarkup = iconMarkup.concat(' ', newIcon);
+      newIcon = ''
+    });
+
+    return iconMarkup;
   }
 }
