25fbbe29f68eb43aeffde9d396eb4c9b1c2fa2e0
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-08-23 11:31:16 +0200

    chore(variablesCustom.css): remove unused CSS rules feat(variablesCustom.css): add custom colors for h1, h2, and h3 feat(variablesCustom.css): set background gradient for body element
    chore(Container.js): remove unused import of style.css
    feat(Container.js): add CSS styles for spacers and media queries for mobile devices

--- src/es/components/atoms/container/Container.js
+++ src/es/components/atoms/container/Container.js
@@ -48,12 +48,47 @@ export default class Container extends Shadow() {
 
   renderCSS () {
     this.css = /* css */`
-        @import url("../../web-components-toolbox/src/css/style.css");
-
         :host {
             max-width: var(--steps-container-max-width, ${this.container.maxWidth});
             padding: var(--steps-container-padding, ${this.container.padding});
             padding-top: 1rem;
+        }
+
+        .spacer, .spacer-one, .spacer-two, .spacer-three, .spacer-four {
+          display: block;
+          height: 1px;
+          margin: var(--spacer-margin, var(--content-spacing) auto) !important;
+          padding: 0;
+        }
+        .spacer-one {
+          margin: var(--spacer-margin, var(--content-spacing) auto 0) !important;
+        }
+        .spacer-two {
+          height: var(--spacer-two-height, calc(2 * var(--content-spacing)));
+        }
+        .spacer-three {
+          height: var(--spacer-three-height, calc(3 * var(--content-spacing)));
+        }
+        .spacer-four {
+          height: var(--spacer-four-height, calc(4 * var(--content-spacing)));
+        }
+
+        @media only screen and (max-width: 767px) {
+          .spacer, .spacer-one, .spacer-two, .spacer-three, .spacer-four {
+            margin: var(--spacer-margin-mobile, var(--spacer-margin, var(--content-spacing-mobile, var(--content-spacing)) auto)) !important;
+          }
+          .spacer-one {
+            margin: var(--spacer-margin-mobile, var(--spacer-margin, var(--content-spacing-mobile, var(--content-spacing)) auto 0)) !important;
+          }
+          .spacer-two {
+            height: var(--spacer-two-height-mobile, var(--spacer-two-height, calc(2 * var(--content-spacing-mobile, var(--content-spacing)))));
+          }
+          .spacer-three {
+            height: var(--spacer-three-height-mobile, var(--spacer-three-height, calc(3 * var(--content-spacing-mobile, var(--content-spacing)))));
+          }
+          .spacer-four {
+            height: var(--spacer-four-height-mobile, var(--spacer-four-height, calc(4 * var(--content-spacing-mobile, var(--content-spacing)))));
+          }
         }
     `
   }
