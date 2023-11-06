5312be2351cf0f83bd8fd841c6570622249c4324
sturudic <srdan.turudic@emea.merkleinc.com>
2023-11-06 10:23:39 +0100

    fix(spielplan): timesuffix string

--- src/es/components/molecules/eventCard/EventCard.js
+++ src/es/components/molecules/eventCard/EventCard.js
@@ -257,7 +257,7 @@ export default class EventCard extends Shadow() {
             <span>${this.event.theater}</span>
           </p>
           <p>
-            ${this.event.eventTime}<br />
+            ${this.event.eventTime} ${this.getAttribute('textTimeSuffix')}<br />
             ${eventIconsHTML}
             ${theaterIconsHTML}
           </p>
